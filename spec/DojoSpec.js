cf30bc29cd77f68b0e2c48cf9683b06d0bbbb56d
Rafael Specht da Silva <rafaelspdasilva@gmail.com>
2013-11-21 14:53:05 -0200

    new round at this tuesday

--- spec/DojoSpec.js
+++ spec/DojoSpec.js
@@ -5,27 +5,37 @@ describe('Boneco', function () {
 	var michaelJackson = new Explorer(cenario.matrix);
 
 	beforeEach(function () {
-
+		cenario.generateMatrix();
 	});
 
-	it('espero que haja uma matriz 5 x 5', function () {
-		expect( cenario.matrix.length ).toBe(5);
-		expect( cenario.matrix[4].length ).toBe(5);
+	it('espero que haja uma matriz com o tamanho certo', function () {
+		
+		expect( cenario.matrix.length ).toBe(cenario.lines);
+		expect( cenario.matrix[0].length ).toBe(cenario.columns);
 	});
 
-	it('espero o indice 3x3 da matriz seja = 1', function () {
-		expect( cenario.matrix[3][3] ).toBe(1);
+	it('espero que o indice da matriz seja = 1 no ponto inserido', function () {
+		var keyPlace = cenario.insertKey();
+		expect(cenario.matrix[keyPlace.line][keyPlace.column]).toBe(1);
+	});
+
+	it('espero que a matriz seja destruida quando eu quiser', function () {
+		cenario.destroyMatrix();
+		expect(cenario.matrix.length).toBe(0);
+
 	});
 
 	it('espero que o metodo find encontre a linha e a coluna o valor = 1', function () {
-		var answer = michaelJackson.find();
-		
-		expect( answer.linha ).toBe(3);
-		expect( answer.coluna ).toBe(3);
-	})
+ 		var answer = michaelJackson.find();
+ 		var position = cenario.insertKey(); 
+ 		console.log(answer)
+		console.log(position)
+	 	expect( answer.line ).toBe(position.line);
+	 	expect( answer.column ).toBe(position.column);
+	 })
 
 	
 	afterEach(function () {
-
+		cenario.destroyMatrix();
 	});
 });
--- src/js/dojo.js
+++ src/js/dojo.js
@@ -1,33 +1,44 @@
 var Cenario = function () {
 	this.init();
 
-
 };
 
-Cenario.prototype.matrix = [
-	[0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0]
-];
-
 Cenario.prototype.init = function () {
-	this.generateMatrix();
-	var explorer = new Explorer(this.matrix);
+	this.matrix = [];
+	this.lines  = Math.round(Math.random()*10) + 1;
+	this.columns = Math.round(Math.random()*10) + 1;
 
+	console.log(this.matrix);
+	var explorer = new Explorer(this.matrix);
 };
 
 Cenario.prototype.generateMatrix = function () {
-	for (var linha = 0; linha < 5; linha += 1) {
-		for (var coluna = 0; coluna < 5; coluna += 1){
-			if (coluna == 3 && linha == 3) {
-				this.matrix[linha][coluna] = 1;
-			}
+	var i,
+		j;
+
+	for (i = 0; i < this.lines; i++) {
+		this.matrix.push([]);
+		for (j = 0; j < this.columns; j++){
+			this.matrix[i].push(0);
 		}
 	}
 };
 
+Cenario.prototype.insertKey = function () {
+	var line  = Math.round(Math.random() * this.lines),
+		column = Math.round(Math.random() * this.columns);
+
+	this.matrix[line][column] = 1;
+
+	return {
+		line: line,
+		column: column
+	}
+};
+
+Cenario.prototype.destroyMatrix = function() {
+	this.matrix = [];
+};
 
 var Explorer = function (matrix) {
 	this.init(matrix);
@@ -39,12 +50,13 @@ Explorer.prototype.init = function(matrix){
 };
 
 Explorer.prototype.find = function () {
-	for (var linha = 0, len = this.matrix.length; linha < len; linha ++) {
-		for (var coluna = 0, linhaLen = this.matrix[linha].length; coluna < linhaLen; coluna++) {
-			if (this.matrix[linha][coluna] === 1) {
-				return {linha: linha, coluna: coluna};
-			} else {
-				return {linha: null, coluna: null};
+	for (var line = 0, len = this.matrix.length; line < len; line ++) {
+		for (var column = 0, linhaLen = this.matrix[line].length; column < linhaLen; column++) {
+			if (this.matrix[line][column] === 1) {
+				return {
+					line: line,
+					column: column
+				}
 			}
 		}
 	}
